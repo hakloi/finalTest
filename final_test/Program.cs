b050639a7f3093f402dbb699631ac6c958ed5b6d
omezhka <murasakitta@gmail.com>
2022-09-20 08:08:26 +0300

    Удаление последнего элемента массива

--- final_test/Program.cs
+++ final_test/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+string [] stingArray = new string[4];
+
+string[] FillArray (string[] arr)
+{
+    Console.WriteLine("Введите элементы массива!");
+    Console.WriteLine();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write("Введите элемент: ");
+        string element = Convert.ToString(Console.ReadLine());
+        arr[i] = element;
+    }
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.WriteLine("Массив: ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+string [] newArray = FillArray(stingArray);
+PrintArray(newArray);
+
+void DeleteTheLastElement(string[] arr)
+{
+    Console.WriteLine();
+    Console.WriteLine("Новый массив: ");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+DeleteTheLastElement(newArray);
